75cd488a986e588bda5faf2a06e148db81d4ecc9
Bini960 <castroandrestruco@gmail.com>
2026-04-06 18:19:28 -0600

    Implementacion de clases e implementacion de la logica de las clases Association y TreenNode (E)

---
+++ src/Association.java
@@ -0,0 +1,56 @@
+/**
+ * Implementacion de Key-Value (K,V).
+ * Adaptada y basada en los conceptos teoricos de:
+ * Bailey, D. A. (2007). Java Structures (7th ed.). Capitulo 12: Trees.
+ */
+public class Association<K extends Comparable<K>, V> implements Comparable<Association<K, V>> {
+    
+    private K key;
+    private V value;
+
+    /**
+     * Construir una nueva asociacion con su llave y valor correspondientes.
+     * * @param key   La llave de busqueda (la palabra en frances)
+     * @param value El valor asociado (la traduccion al espanol)
+     */
+    public Association(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    /**
+     * Obtener la llave de la asociacion.
+     * * @return La llave actual
+     */
+    public K getKey() {
+        return key;
+    }
+
+    /**
+     * Obtener el valor de la asociacion.
+     * * @return El valor actual
+     */
+    public V getValue() {
+        return value;
+    }
+
+    /**
+     * Modificar el valor de la asociacion.
+     * * @param value El nuevo valor a asignar
+     */
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    /**
+     * Comparar esta asociacion contra otra basandose en la llave.
+     * Esto es necesario para que el arbol binario sepa como ordenar los nodos.
+     * * @param other La otra asociacion a comparar
+     * @return un numero negativo, cero, o positivo segun el orden alfabetico
+     */
+    @Override
+    public int compareTo(Association<K, V> other) {
+        // Delegamos la comparacion al metodo compareTo de la llave (K)
+        return this.key.compareTo(other.getKey());
+    }
+}
---
+++ src/TreeNode.java
@@ -0,0 +1,71 @@
+/**
+ * Clase para los nodos de un Arbol Binario.
+ * Adaptada y basada en los conceptos teoricos de:
+ * Bailey, D. A. (2007). Java Structures (7th ed.). Capitulo 12: Trees.
+ *
+ * @param <E> El tipo de dato que guardara el nodo (Elemento generico)
+ */
+public class TreeNode<E> {
+    
+    private E value;
+    private TreeNode<E> left;
+    private TreeNode<E> right;
+
+    /**
+     * Construir un nuevo nodo hoja (sin hijos) con un valor especifico.
+     * * @param value El dato a almacenar en el nodo
+     */
+    public TreeNode(E value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    /**
+     * Obtener el valor almacenado en este nodo.
+     * * @return El valor actual del nodo
+     */
+    public E getValue() {
+        return value;
+    }
+
+    /**
+     * Modificar el valor almacenado en este nodo.
+     * * @param value El nuevo valor a asignar
+     */
+    public void setValue(E value) {
+        this.value = value;
+    }
+
+    /**
+     * Obtener el hijo izquierdo de este nodo.
+     * * @return El nodo hijo izquierdo (puede ser null)
+     */
+    public TreeNode<E> getLeft() {
+        return left;
+    }
+
+    /**
+     * Asignar un nodo como hijo izquierdo.
+     * * @param left El nodo que sera el nuevo hijo izquierdo
+     */
+    public void setLeft(TreeNode<E> left) {
+        this.left = left;
+    }
+
+    /**
+     * Obtener el hijo derecho de este nodo.
+     * * @return El nodo hijo derecho (puede ser null)
+     */
+    public TreeNode<E> getRight() {
+        return right;
+    }
+
+    /**
+     * Asignar un nodo como hijo derecho.
+     * * @param right El nodo que sera el nuevo hijo derecho
+     */
+    public void setRight(TreeNode<E> right) {
+        this.right = right;
+    }
+}
